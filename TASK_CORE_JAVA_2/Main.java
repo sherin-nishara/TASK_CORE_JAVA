ffa7e7077c378bb915b1b032363952f2981d2711
sherin-nishara <sherinnishara@outlook.com>
2025-05-21 22:08:43 +0530

    Update Main.java

--- TASK_CORE_JAVA_2/Main.java
+++ TASK_CORE_JAVA_2/Main.java
@@ -1,45 +1,112 @@
+
+package tic;
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
-class Main {
-    private int ticketId;
-    private String passengerName;
-    private String source;
-    private String destination;
-    private String travelDate;
+
+class Ticket {
+    String name;
+    String port;
+    String dest;
+    String date;
+    static int count = 1;
     
-    static{
-        System.out.println("--------Ticketing System--------");
+    Ticket(String name, String port, String dest, String date){
+        this.name = name;
+        this.port = port;
+        this.dest = dest;
+        this.date = date;
+        count++;
+    }
+    
+    public static void book(Scanner sc, HashMap<Integer,Ticket> rec){
+        System.out.print("name : ");
+        String name = sc.nextLine();
+        System.out.print("source : ");
+        String port = sc.nextLine();
+        System.out.print("destination : ");
+        String dest = sc.nextLine();
+        System.out.print("date : ");
+        String date = sc.nextLine();
+        int id = count;
+        Ticket tic = new Ticket(name,port,dest,date);
+        rec.put(id,tic);
+        System.out.println("created");
+    }
+    
+    public static void view(HashMap<Integer,Ticket> rec){
+        for(Map.Entry<Integer,Ticket> s: rec.entrySet()){
+            System.out.println("--------------\n"+"ticket Id : " + s.getKey() + " \n" + s.getValue());
+        }
+    }
+    
+    public static void byId(Scanner sc, HashMap<Integer,Ticket> rec){
+        System.out.print("enter ticketId : ");
+        int search = sc.nextInt();
+        sc.nextLine();
+        for(Map.Entry<Integer,Ticket> s: rec.entrySet()){
+            if(search == s.getKey()){
+                System.out.println(s.getValue());
+            }else{
+                System.out.println("id not found");
+            }
+        }
+    }
+    
+    public static void update(Scanner sc, HashMap<Integer,Ticket> rec){
+        System.out.println("enter id : ");
+        int search = sc.nextInt();
+        sc.nextLine();
+        for(Map.Entry<Integer,Ticket> s: rec.entrySet()){
+            if(search == s.getKey()){
+                System.out.println("name : ");
+                String name = sc.nextLine();
+                System.out.println("from : ");
+                String port = sc.nextLine();
+                System.out.println("dest : ");
+                String dest = sc.nextLine();
+                System.out.println("date : ");
+                String date = sc.nextLine();
+                
+                Ticket newTic = new Ticket(name,port,dest,date);
+                
+                rec.put(search,newTic);
+            }else{
+                System.out.println("id not found");
+            }
+        }
+        
+    }
+    @Override
+    public String toString(){
+        return " name : "+ name+" \n source: "+port+" \n destination: "+ dest+" \n date: "+ date+ "\n---------------";
     }
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer,Main> records = new HashMap<>();
-        boolean exit = true;
-        while(exit){
-            System.out.print("Enter choice by number\n 1)Book ticket\n 2)View by ID\n 3)View all\n 4)Update ticket\n 5)Cancel ticket\n 6)exit\n Enter number : ");
-            int choice = sc.nextInt();
+        HashMap<Integer,Ticket> rec = new HashMap<>();
+        
+        boolean choice = true;
+        while(choice){
+            System.out.println("1)book 2)view 3)view by id 4)update 5)cancel \n 6)exit");
+            int c = sc.nextInt();
             sc.nextLine();
-            switch(choice){
+            switch(c){
                 case 1:
-                    bookTic();
+                    book(sc,rec);
                     break;
                 case 2:
-                    viewById();
+                    view(rec);
                     break;
                 case 3:
-                    viewTic();
+                    byId(sc,rec);
                     break;
                 case 4:
-                    updateTic();
-                    break;
-                case 5:
-                    cancelTic();
+                    update(sc,rec);
                     break;
                 case 6:
-                    exit = false;
+                    choice = false;
                     break;
-                default:
-                    System.out.println("Please enter provied numbers only");
             }
         }
     }
